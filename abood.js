1c7b8d953b2f690db84a5fd0168a7b35d9100e52
Abood7783 <Aboodalahdal7783@gmail.com>
2025-02-19 21:56:51 +0300

    abood.js

--- abood.js
+++ abood.js
@@ -172,3 +172,184 @@ setInterval(function () {
         } 
     } 
 }, 10);
+
+
+
+
+
+
+
+$(`<style>
+.room.borderg.hand.nosel.fl.ri1scui57fs img.fl.u-pic {
+display: none;
+}
+.room.borderg.hand.nosel.fl.ri1scui57fs .fl.u-pic.borderg {
+  display: none;
+letter-spacing: 1px;
+  background-size: 108% 108%;
+  -webkit-text-fill-color: white;
+  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+}
+.room.borderg.hand.nosel.fl.ri1scui57fs .fa.fa-user.label.label-primary.fr.uc {
+  letter-spacing: 1px;
+  -webkit-text-fill-color: #fff;
+  background-image: url(https://up6.cc/2025/01/173792471321671.png);
+  background-size: 100% 100%;
+  height: 27px;
+  margin-top: -2px !important;
+  margin-bottom: 0px !important;
+  margin-left: 45px !important;
+  text-align: left !important;
+}
+.room.borderg.hand.nosel.fl.ri1scui57fs.bord {border-inline: 4px solid #4c121e !important;}
+.room.borderg.hand.nosel.fl.ri1scui57fs {
+border-radius: 10px 0px 10px 0px;
+display: inline-block;
+background-image: linear-gradient(-225deg, rgb(164, 164, 164) 0%, rgba(255, 0, 143, 0) 29%, rgba(151, 101, 127, 0.04) 67%, rgb(189, 189, 189) 100%);
+}
+.room.borderg.hand.nosel.fl.ri1scui57fs .fl.mini.u-msg {
+display: none;
+  text-align: center;
+  -webkit-text-fill-color: #ffffff70;
+  background-image: url(https://up6.cc/2023/01/167382398143361.gif);
+  -webkit-background-clip: text;
+  color: transparent !important;
+  background-size: 100% 100%;
+  margin-top: 5px !important;
+  margin-left: 0px;
+}
+.room.borderg.hand.nosel.fl.ri1scui57fs {
+  border-radius: 5px;
+  background-image: url(https://up6.cc/2025/02/173997005109251.png);
+  
+  background-size: 100% 100%;
+}
+.room.borderg.hand.nosel.fl.ri1scui57fs .fl.u-pic.borderg {
+
+  border-radius: 100px;
+  border: 2px solid #cc8321;
+  letter-spacing: 1px;
+  background-size: 108% 108%;
+  -webkit-text-fill-color: white;
+  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+  min-width: 62px !important;
+  width: 62px !important;
+  min-height: 60px !important;
+  max-height: 60px !important;
+  display: none;
+}
+.room.borderg.hand.nosel.fl.ri1scui57fs .u-topic.dots {
+
+  text-transform: uppercase;
+  -webkit-background-clip: text;
+color: transparent !important;
+  animation: textclip 2s linear infinite;
+  display: inline-block;
+  font-size: 22px !important;
+  
+  -webkit-background-clip: text;
+  color: transparent !important;
+  background-size: 100% 100%;
+  margin-top: 50%;
+  margin-left: 20px;
+}
+@keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
+var rid='i1scui57fs';
+$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{animation: textclip 5s linear infinite;
+border-radius: 50px !important;
+border: 1px solid #0000;
+background-image: url(https://up6.cc/2023/01/16738239819832.gif);
+-webkit-background-clip: text;
+color: transparent !important;
+background-size: 100% 100%;
+margin-top: -3px;}`);
+$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{padding: 2px !important;
+animation: textclip 5s linear infinite;
+border-radius: 50px !important;
+border: 1px solid #0000;
+background-image: url(https://up6.cc/2023/01/16738239819832.gif);
+-webkit-background-clip: text;
+color: transparent !important;
+background-size: 100% 100%;
+margin-top: -5px;}`);
+$(`<style>#mic.tiir-baht3 .mic {border: 2px solid;border-image: linear-gradient(var(--angle), #e18b18 0%, #000 29%, #cc8321 67%, #020202 100%) 1;animation: 5s rotate linear infinite;--angle: 0deg;background-color: #e3e3e3;border-radius: 5px !important;margin: 2px;margin-top: 1px;box-shadow: inset 0 0px 0px rgb(0 0 0 / 8%), 0 0px 1px #ffffff;height: 48px;}@keyframes rotate {
+    to {
+        --angle: 360deg;
+    }
+}
+@property --angle {
+    syntax: "<angle>";
+    initial-value: 0deg;
+    inherits: false;
+}</style>`).insertBefore('body');
+$("style").last().append(`
+#users.tiir-baht1 .inroom.uzr {
+  border-radius: 10px 0px 10px 0px;
+  background-size: cover;
+}
+#d0.tiir-baht1 .label-primary {
+  border: 1px solid #ADD8e6 !important;
+  border-radius: 10px 0px 10px 0px !important;
+}
+#room.tiir-baht1 .btn-primary {
+  border: 1px solid #ADD8e6 !important;
+  border-radius: 10px 0px 10px 0px !important;
+}
+
+#users.tiir-baht1 .inroom.uzr img.ustat {
+  width: 5px !important;
+  border-radius: 10px 0px 15px 0px;
+  height: 5px;
+  margin-top: -1px;
+  min-height: 0% !important;
+}
+#users.tiir-baht1 .inroom.uzr .fitimg.u-pic {
+  border-radius: 10px 0px 10px 0px;
+margin-top: 0px;
+z-index: 0;
+margin-left: -51px;
+min-width: 50px;
+width: 50px;
+min-height: 48px;
+max-height: 50px;
+}
+#d2.tiir-baht2 .uzr .fitimg.u-pic{
+border:1px solid #ADD8e6;
+margin-top:2px;
+border-radius: 10px 0px 10px 0px;
+box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #add8e6;}
+#users.tiir-baht1 .inroom.uzr .u-msg{
+-webkit-text-fill-color: #555;
+}
+#users.tiir-baht1 .inroom.uzr span.uhash{
+text-transform:uppercase;
+background-image: linear-gradient(-225deg,#7b7b7b 0,#737373 29%,#add8e6 67%,#996013 100%);
+background-size:220% auto;
+-webkit-background-clip:text;
+-webkit-text-fill-color:transparent;animation:2s linear infinite textclip;
+display:inline-block}#users.tiir-baht1 .inroom.uzr img.co{transform:rotate(20deg);margin-top:2px!important}
+#d2.tiir-baht2 .uzr.d-flex.mm{
+border-radius: 10px 0px 10px 0px;
+border:1px solid #fffefe;
+border-bottom:1px solid #fffefe}
+`);
+
+
+var r_tiirbaht = false;
+setInterval(function () {
+    if (myroom == 'i1scui57fs') {
+        if (r_tiirbaht == false) {
+            r_tiirbaht = true;
+            $('#users').addClass('tiir-baht');
+            $('#d2').addClass('tiir-baht2');
+            $('#mic').addClass('tiir-baht3');
+        }
+    } else {
+        if (r_tiirbaht == true) {
+            r_tiirbaht = false;
+            $("#users").removeClass('tiir-baht1');
+            $("#d2").removeClass('tiir-baht2');
+            $('#mic').removeClass('tiir-baht3');
+        } 
+    } 
+}, 10);
